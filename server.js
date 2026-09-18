23418b78c480ef15ab33185ea0d288c24794c477
Art Jørstad Olson <64359626+Artomic2020@users.noreply.github.com>
2026-09-19 00:49:04 +0200

    Set up Node and Express

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+require("dotenv").config();
 const express = require("express");
+const mysql = require("mysql2")
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+});
+
+db.connect((err) =>{
+    if (err) {
+        console.error("Database connection failed:", err);
+        return;    
+    }
+
+    console.log("Connected to MySQL!")
+})
+
 
 app.get("/", (req, res) => {
     res.send("PROG2052 Node server is running!");
